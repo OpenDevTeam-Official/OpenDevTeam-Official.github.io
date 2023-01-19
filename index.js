dc1227d1710e4063316e3d1b30dda6ce64d3b30d
Simon <simon1240.se@gmail.com>
2023-01-19 20:56:06 +0100

    save

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+var audio = new Audio('nyan.mp3');
+
 function nyanButton() {
+  // change body css
+  document.body.style.cssText += 'background: linear-gradient(-45deg, #ffeb3b, #74efff, #9c27b0); background-size: 100% 100%; animation: gradient-bg 1s ease infinite;';
+
   // play nyan cat song
-  var audio = new Audio('nyan.mp3');
-  audio.play();
+  //audio.play();
 }
