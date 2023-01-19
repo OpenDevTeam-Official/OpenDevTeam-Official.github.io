a44fa12dca6db40569572a29233faad83f909cc7
AlmosYT <almosytpixelplace@gmail.com>
2023-01-19 22:15:48 +0100

    small fixes

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var audio = new Audio('nyan.m4a');
 
 function nyanButton() {
-  // change the title itself to black so its readable
-
   document.getElementById('odt').innerHTML= "NyanDT";
   document.body.style.cssText += 'background: linear-gradient(124deg, #ff2400, #e81d1d, #e8b71d, #e3e81d, #1de840, #1ddde8, #2b1de8, #dd00f3, #dd00f3); background-size: 1200% 1200%; -webkit-animation: rainbow 20s ease infinite; animation: rainbow 20s ease infinite;';
 
